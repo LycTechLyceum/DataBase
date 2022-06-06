ee7b12401cc12c928014797b6a4e3a7b5bcd491b
LycTechLyceum <106927909+LycTechLyceum@users.noreply.github.com>
2022-06-06 23:42:58 +0300

    Update db_session.py

--- data/db_session.py
+++ data/db_session.py
@@ -11,6 +11,9 @@ SqlAlchemyBase = dec.declarative_base()
 
 __factory = None
 
+''' Создает базу данных при первом вызове по данному адресу,
+если она уже была вызвана ранее, то ничего не создает
+необходима для инициализации БД через код'''
 
 def global_init(db_file):
     global __factory
@@ -18,13 +21,13 @@ def global_init(db_file):
     if __factory:
         return
 
-    if not db_file or not db_file.strip():
+    if not db_file or not db_file.strip(): # проверяем, что у нас не пустой адрес
         raise Exception("Необходимо указать файл базы данных.")
 
-    conn_str = f'sqlite:///{db_file.strip()}?check_same_thread=False'
+    conn_str = f'sqlite:///{db_file.strip()}?check_same_thread=False' # создаем строку подключения к БД
     print(f"Подключение к базе данных по адресу {conn_str}")
 
-    engine = sa.create_engine(conn_str, echo=False)
+    engine = sa.create_engine(conn_str, echo=False) # создаем движок, с которым и будем работать впоследствии
     __factory = orm.sessionmaker(bind=engine)
 
     from . import __all_models
@@ -32,6 +35,6 @@ def global_init(db_file):
     SqlAlchemyBase.metadata.create_all(engine)
 
 
-def create_session() -> Session:
+def create_session() -> Session: # создаем все объекты, которые еще не были созданы
     global __factory
     return __factory()
